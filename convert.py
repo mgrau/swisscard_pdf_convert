1395932d75be2fd6b93eb53285e5dfc606e6b2a3
Matt Grau <graum@Lutetium.localdomain>
2021-08-27 16:57:13 +0200

    changed match string to fit a wider range of transactions

--- convert.py
+++ convert.py
@@ -14,7 +14,7 @@ with pdfplumber.open(input_file) as pdf:
 lines = text.split('\n')
 
 
-parse_str = '^(?P<date>(\d\d\.){2}\d{4}) (?P<transaction>\S*) (?P<credit>-)?(?P<amount>(\'|\d)+\.\d\d)$'
+parse_str = '^(?P<date>(\d\d\.){2}\d{4}) (?P<transaction>\S.*\S) (?P<credit>-)?(?P<amount>(\'|\d)+\.\d\d)$'
 prog = re.compile(parse_str)
 
 transactions = [line for line in lines if prog.match(line) is not None]
